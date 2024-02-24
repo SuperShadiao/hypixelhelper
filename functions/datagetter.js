3b3dc76b79d6f571f6326c23e146ff18eab9cac8
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 15:52:51 +0800

    Update datagetter.js

--- functions/datagetter.js
+++ functions/datagetter.js
@@ -1,4 +1,4 @@
 export function onRequest(context) {
-  let url = context.request.url.searchParams.get('url');
+  let url = new URL(context.request.url).searchParams.get('url');
   return fetch(new URL( url ))
 }
