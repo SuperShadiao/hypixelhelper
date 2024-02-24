85c07adceca9549ad6f330071c7cede1033d073e
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 10:50:40 +0800

    Update ntyl.js

--- functions/ntyl.js
+++ functions/ntyl.js
@@ -7,7 +7,7 @@ export async function onRequest(context) {
   do {
     i++
     res1 = await context.env.ASSETS.fetch(new URL(context.request.url))
-  } while(i < 100 || res1.status == 404)
+  } while(i < 100 && res1.status == 404)
   
   let res = new Response("redirect", {
     status: 301
