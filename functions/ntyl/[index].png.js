6c732641cb2378237007e5cfbb7d051a2e6f94cb
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 10:34:06 +0800

    Create [index].png.js

---
+++ functions/ntyl/[index].png.js
@@ -0,0 +1,9 @@
+async function onRequest(context) {
+
+  let i = context.params.index
+
+  let res
+  res = await context.env.ASSETS.fetch(new URL(context.request.url))
+  
+  return new Response("Code: " + res.status)
+}
