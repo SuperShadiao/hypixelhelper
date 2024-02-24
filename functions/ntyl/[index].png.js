22ef1e0f8c073a464d5b97551b4a63b6e4c05c8a
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 10:08:30 +0800

    Update [index].png.js

--- functions/ntyl/[index].png.js
+++ functions/ntyl/[index].png.js
@@ -1,9 +1,10 @@
 export function onRequest(context) {
-  let res1 = context.next()
+  // let res1 = context.next()
   // if(res1.status == 404) {
   //  return new Response("Code: " + res1.status)
   // }
   // return res1
   let res2 = context.env.ASSETS.fetch(new URL(context.request.url))
-  return res2
+  
+  return new Response("Code: " + res2.status)
 }
