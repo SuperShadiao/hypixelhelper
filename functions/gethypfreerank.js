bca0281262229b2f6737cd3ca5ed3045ae3601a6
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 12:54:52 +0800

    Update gethypfreerank.js

--- functions/gethypfreerank.js
+++ functions/gethypfreerank.js
@@ -1,11 +1,11 @@
-export async function onRequest(context) {
+export function onRequest(context) {
 
   let a = [
     "https://ys.mihoyo.com/",
     "https://www.bilibili.com/video/BV1uT4y1P7CX"
   ]
   
-  return context.env.ASSETS.fetch(new URL(
+  return fetch(new URL(
     a [Math.floor(Math.random() * 2)]
   ))
   
