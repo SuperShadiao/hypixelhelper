ac0126fdf828977e84fa865f51a127772fbec307
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 11:22:48 +0800

    Update ZZ2Z_Lore.js

--- functions/ZZ2Z_Lore.js
+++ functions/ZZ2Z_Lore.js
@@ -5,7 +5,7 @@ export async function onRequest(context) {
   let res1
   let url0 = new URL(context.request.url)
 
-  if(parseInt(context.env.zz2z_lore_pic_count) == 0 || Math.random() > 0.85) {
+  if(parseInt(context.env.zz2z_lore_pic_count) == 0 || (Math.random() > 0.85 && parseInt(context.env.flushflag) > 10)) {
     do {
       i++
       let url1 = new URL(context.request.url)
@@ -13,6 +13,9 @@ export async function onRequest(context) {
       res1 = await context.env.ASSETS.fetch(url1)
     } while(i < 100 && res1.status == 200)
     context.env.zz2z_lore_pic_count = i
+  } else {
+    i = context.env.zz2z_lore_pic_count
+    context.env.flushflag = parseInt(context.env.flushflag) + 1
   }
   let res = new Response("redirect", {
     status: 301
