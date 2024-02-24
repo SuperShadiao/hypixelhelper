6ed157cfd00fc042bab779c3fe07a8a9e71a14a6
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 10:20:07 +0800

    Update ntyl.js

--- functions/ntyl.js
+++ functions/ntyl.js
@@ -4,7 +4,7 @@ export function onRequest(context) {
     status: 301
   })
 
-  res.headers.append("Location", new URL(context.request.url).pathname + "/" + Math.floor(Math.random() * 3) + ".png")
+  res.headers.append("Location", new URL(context.request.url).pathname + "/" + (Math.floor(Math.random() * 2) + 1) + ".png")
 
   return res
 }
