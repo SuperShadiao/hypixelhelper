625dce7c4c0f237d5ac8f0eebff2a13a0673dd0c
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 16:31:03 +0800

    Create [type].js

---
+++ functions/hypixelapikeymanager/[type].js
@@ -0,0 +1,30 @@
+export function onRequest(context) {
+
+  if(context.params.type == "puttempkey") {
+    let jsonObj = JSON.parse(context.env.hypixelkey)
+  
+    let jsonObjKeyInfo = JSON.parse('{}')
+  
+    if(!(jsonObj.keys instanceof Array)) {
+      jsonObj.keys = []
+    }
+    
+    let url = new URL(context.request.url)
+    let key = url.searchParams.get('key');
+    let time = Date.now()
+  
+    jsonObjKeyInfo.key = key
+    jsonObjKeyInfo.time = time
+  
+    jsonObj.keys[jsonObj.length] = jsonObjKeyInfo
+
+    context.env.hypixelkey = JSON.stringify(jsonObj)
+    
+    return new Response("成功啦", {status: 200})
+  } else if(context.params.type == "get") {
+    return new Response(context.env.hypixelkey, {status: 200})
+  } else {
+    return new Response("别吵, 我在思考", {status: 403})
+  }
+  
+}
