54bc0f2c336ad368fcc72a806bc83f69bc0e55d5
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 11:11:29 +0800

    Update helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,6 +1,7 @@
 export function onRequest(context) {
 
+  let old = context.env.oi
   context.env.oi = Math.random()
   
-  return new Response("Hello, world! " + context.env.oi)
+  return new Response(old + "Hello, world! " + context.env.oi)
 }
