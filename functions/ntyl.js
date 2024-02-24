58abc4ac146cb5b03189eee25d478b9a47eedd5e
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-02-24 10:47:52 +0800

    Update ntyl.js

--- functions/ntyl.js
+++ functions/ntyl.js
@@ -1,10 +1,19 @@
-export function onRequest(context) {
+export async function onRequest(context) {
 
+  let i = 0
+
+  let res1
+
+  do {
+    i++
+    res1 = await context.env.ASSETS.fetch(new URL(context.request.url))
+  } while(i < 100 || res1.status == 404)
+  
   let res = new Response("redirect", {
     status: 301
   })
 
-  res.headers.append("Location", new URL(context.request.url).pathname + "/" + (Math.floor(Math.random() * 2) + 1) + ".png")
+  res.headers.append("Location", new URL(context.request.url).pathname + "/" + (Math.floor(Math.random() * i) + 1) + ".png")
 
   return res
 }
