a478716804eceee8a3d2e6a32a91abe5bf1825cb
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-03-16 05:30:23 +0800

    Update gethypfreerank.js

--- functions/gethypfreerank.js
+++ functions/gethypfreerank.js
@@ -1,19 +1,20 @@
 export function onRequest(context) {
 
-  let a = [
-    "https://ys.mihoyo.com/",
-    "https://www.bilibili.com/video/BV1uT4y1P7CX"
-  ]
+  // let a = [
+  //   "https://ys.mihoyo.com/",
+  //   "https://www.bilibili.com/video/BV1uT4y1P7CX"
+  // ]
 
-  let s = a [Math.floor(Math.random() * 2)]
-  if(s.includes("bilibili")) {
-    let res = new Response("redirect", {
-      status: 301
-    })
+  // let s = a [Math.floor(Math.random() * 2)]
+  // if(s.includes("bilibili")) {
+  //   let res = new Response("redirect", {
+  //     status: 301
+  //   })
   
-    res.headers.append("Location", s)
+  //   res.headers.append("Location", s)
   
-    return res
-  } else return fetch(new URL( s ))
+  //   return res
+  // } else return fetch(new URL( s ))
+  return context.next()
   
 }
