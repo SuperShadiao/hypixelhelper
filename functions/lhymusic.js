22a214cd407a876e1f7291736b2724d286c525d9
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-03-22 21:37:38 +0800

    Update lhymusic.js

--- functions/lhymusic.js
+++ functions/lhymusic.js
@@ -3,6 +3,6 @@ export async function onRequest(context) {
   let type = url.searchParams.get('type');
   let name = url.searchParams.get('name');
 
-  let key = JSON.parse(fetch("https://hypixelhelper.pages.dev/e.json")) ["api.linhun.vip.qqmusic.api.accesskey"]
-  return fetch(new URL("https://api.linhun.vip/api/qqyy?name=" + name + "&y=1&n=1&apiKey=" + key))
+  let key = JSON.parse(await fetch("https://hypixelhelper.pages.dev/e.json")) ["api.linhun.vip.qqmusic.api.accesskey"]
+  return await fetch(new URL("https://api.linhun.vip/api/qqyy?name=" + name + "&y=1&n=1&apiKey=" + key))
 }
