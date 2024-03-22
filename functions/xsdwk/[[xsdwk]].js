d17c849fec7421aca39e66bedecaf458c06dde08
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-03-22 20:30:16 +0800

    Update [[xsdwk]].js

--- functions/xsdwk/[[xsdwk]].js
+++ functions/xsdwk/[[xsdwk]].js
@@ -1,5 +1,6 @@
 export function onRequest(context) {
   let url = new URL(context.request.url);
-  
-  return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(5) + url.search))
+
+  return new Respnose(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(5) + url.search).href)
+  // return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(5) + url.search))
 }
