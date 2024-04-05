911611b96d5074d8ad21d789972fae7132869166
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-04-05 08:33:13 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   try {
     const res = await context.next();
-    if(res == 404) return new Response("ei", 200)
+    return res
   } catch (err) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
   }
