52efe6eea2fefd6046001817528bc39ced52151e
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-04-19 19:27:45 +0800

    Update ZZ2Z_Lore.js

--- functions/ZZ2Z_Lore.js
+++ functions/ZZ2Z_Lore.js
@@ -1,6 +1,7 @@
 export async function onRequest(context) {
 
   let i = 0
+  let indexText = await ((await context.env.ASSETS.fetch(new URL("https://hypixelhelper.pages.dev"))).text())
 
   let res1
   let url0 = new URL(context.request.url)
@@ -11,7 +12,7 @@ export async function onRequest(context) {
     let url1 = new URL(context.request.url)
     url1.pathname = url0.pathname + "/" + Math.floor(picCount * Math.random()) + ".jpg"
     res1 = await context.env.ASSETS.fetch(url1)
-  } while(i < 100 && res1.status != 200)
+  } while(i < 100 && await (res1.text()) == indexText)
 
   return new Response(JSON.stringify(context.env.ASSETS) + "")
   return res1
