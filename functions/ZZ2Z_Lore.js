397d380abd194074eb5038cd4662a557b297a549
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-04-19 18:58:39 +0800

    Update ZZ2Z_Lore.js

--- functions/ZZ2Z_Lore.js
+++ functions/ZZ2Z_Lore.js
@@ -4,23 +4,16 @@ export async function onRequest(context) {
 
   let res1
   let url0 = new URL(context.request.url)
+  const picCount = 200
 
-  if(parseInt(context.env.zz2z_lore_pic_count) == 0 || (Math.random() > 0.85 && parseInt(context.env.flushflag) > 10)) {
-    do {
-      i++
-      let url1 = new URL(context.request.url)
-      url1.pathname = url0.pathname + "/" + i + ".jpg"
-      res1 = await context.env.ASSETS.fetch(url1)
-    } while(i < 100 && res1.status == 200)
-    context.env.zz2z_lore_pic_count = i
-  } else {
-    i = context.env.zz2z_lore_pic_count
-    context.env.flushflag = parseInt(context.env.flushflag) + 1
-  }
-  i--
-  let url2 = new URL(context.request.url)
-  url2.pathname = url0.pathname + "/" + (Math.floor(Math.random() * i) + 1) + ".jpg"
-  return context.env.ASSETS.fetch(url2)
+  do {
+    i++
+    let url1 = new URL(context.request.url)
+    url1.pathname = url0.pathname + "/" + Math.floor(picCount * Math.random()) + ".jpg"
+    res1 = await context.env.ASSETS.fetch(url1)
+  } while(i < 100 && res1.status == 404)
+    
+  return res1
 }
 
   // let res = new Response("redirect", {
