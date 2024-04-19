91eecb5ead49bf766e6a672cbdf5471cd9bb96f3
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-04-19 19:22:34 +0800

    Update ZZ2Z_Lore.js

--- functions/ZZ2Z_Lore.js
+++ functions/ZZ2Z_Lore.js
@@ -12,7 +12,8 @@ export async function onRequest(context) {
     url1.pathname = url0.pathname + "/" + Math.floor(picCount * Math.random()) + ".jpg"
     res1 = await context.env.ASSETS.fetch(url1)
   } while(i < 100 && res1.status != 200)
-    
+
+  return new Response(context.env.ASSETS + "")
   return res1
 }
 
