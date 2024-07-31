d332bdcd8abc138aa96b24857dc8f2296cecf0b4
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-08-01 05:18:52 +0800

    Update [[xsdwk]].js

--- functions/xsdwk/[[xsdwk]].js
+++ functions/xsdwk/[[xsdwk]].js
@@ -1,13 +1,15 @@
 export function onRequestPost(context) {
   let url = new URL(context.request.url);
 
-  // return new Response(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(5) + url.search).href)
-  return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(7) + url.search), {method: "POST", body: context.request.body/*, headers: context.request.headers*/})
+  let headers = context.request.headers
+  headers["cf-ipcountry"] = "SG"
+  return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(7) + url.search), {method: "POST", body: context.request.body, headers: headers})
 }
 
 export function onRequest(context) {
   let url = new URL(context.request.url);
 
-  // return new Response(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(5) + url.search).href)
-  return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(7) + url.search), {/*headers: context.request.headers*/})
+  let headers = context.request.headers
+  headers["cf-ipcountry"] = "SG"
+  return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(7) + url.search), {headers: headers})
 }
