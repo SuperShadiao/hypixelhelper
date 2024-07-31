b254acd1ed25993ecde71ebe8aa8426fbb5dc279
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-08-01 04:59:56 +0800

    Update datagetter.js

--- functions/datagetter.js
+++ functions/datagetter.js
@@ -1,4 +1,12 @@
 export function onRequest(context) {
   let url = new URL(context.request.url).searchParams.get('url');
-  return fetch(new URL( url ))
+  return fetch(new URL( url ), {headers: context.request.headers})
+}
+
+export function onRequestPost(context) {
+  let url = new URL(context.request.url).searchParams.get('url');
+
+  return fetch(new URL( url ), {method: "POST", body: context.request.body, headers: context.request.headers})
+  // return new Response(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(5) + url.search).href)
+  // return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(7) + url.search), {method: "POST", body: context.request.body, headers: context.request.headers})
 }
