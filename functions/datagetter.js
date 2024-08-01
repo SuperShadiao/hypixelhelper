94156ed0b42b7efbaf235b9ba70726162c2d5320
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2024-08-01 11:26:54 +0800

    Update datagetter.js

--- functions/datagetter.js
+++ functions/datagetter.js
@@ -1,19 +1,11 @@
 export function onRequest(context) {
   let url = new URL(context.request.url).searchParams.get('url');
   
-  let headers = context.request.headers
-  if(url.search("api.groq.com") != -1) headers["cf-ipcountry"] = "SG"
-  
-  return fetch(new URL( url )/*, {headers: headers}*/)
+  return fetch(new URL( url ), {context.request.headers})
 }
 
 export function onRequestPost(context) {
   let url = new URL(context.request.url).searchParams.get('url');
-
-  let headers = context.request.headers
-  if(url.search("api.groq.com") != -1) headers["cf-ipcountry"] = "SG"
   
-  return fetch(new URL( url ), {method: "POST", body: context.request.body, headers: headers})
-  // return new Response(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(5) + url.search).href)
-  // return fetch(new URL("https://hh.xiaoshadiao.workers.dev/" + url.pathname.substr(7) + url.search), {method: "POST", body: context.request.body, headers: context.request.headers})
+  return fetch(new URL( url ), {method: "POST", body: context.request.body, headers: context.request.headers})
 }
