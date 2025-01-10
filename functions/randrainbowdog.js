6474bac9bdc50ab995faca7bd65a1ee02113d995
Super_Shadiao <61408108+SuperShadiao@users.noreply.github.com>
2025-01-11 07:32:00 +0800

    Update randrainbowdog.js

--- functions/randrainbowdog.js
+++ functions/randrainbowdog.js
@@ -10,7 +10,7 @@ export async function onRequest(context) {
   do {
     i++
     let url1 = new URL(context.request.url)
-    url1.pathname = url0.pathname + "/" + Math.floor(picCount * Math.random()) + ".gif"
+    url1.pathname = "https://hypixelhelper.pages.dev/rainbowdogs/" + Math.floor(picCount * Math.random()) + ".gif"
     res1 = await context.env.ASSETS.fetch(url1)
   } while(i < 100 && await (res1.clone().text()) == indexText)
 
