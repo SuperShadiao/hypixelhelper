21b9761d8d6025bdadf00fffe7bf7e586cd8f150
SuperShadiao <3381949033@qq.com>
2025-10-18 17:20:22 +0800

    关于页面更新awa

---
+++ functions/go.js
@@ -0,0 +1,23 @@
+export function onRequest(context) {
+    let id = new URL(context.request.url).searchParams.get('id');
+
+    let response = null
+    switch (id) {
+        case '1':
+            response = Response.redirect("https://github.com/ABCOA/Legacy-SoarClient");
+            break;
+        case '2':
+            response = Response.redirect("https://modrinth.com/mod/yqlossclientmixin");
+            break;
+        case '3':
+            response = Response.redirect("https://space.bilibili.com/524241250");
+            break;
+    }
+    if (response == null) {
+        return Response.json({
+            "success": false,
+            "msg": "小沙雕翻遍了整个崩坏星穹铁道也找不着东西哇qwq"
+        }, { status: 404 });
+    }
+    return response;
+}
