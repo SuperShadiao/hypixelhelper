204dcdd6a54b4274ea2dffd9b4bdd7b1e5e4de0a
SuperShadiao <3381949033@qq.com>
2025-10-20 18:03:34 +0800

    写了一万年的底部栏终于给我弄好了:<)

--- sitesources/js/header.js
+++ sitesources/js/header.js
@@ -1,18 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('/sitesources/html/header.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            const headerPlaceholder = document.getElementById('header-placeholder');
-            if (headerPlaceholder) {
-                headerPlaceholder.innerHTML = data;
-            }
-        })
-        .catch(error => {
-            console.error('Error loading header:', error);
-        });
+    Promise.all([
+        fetch('/sitesources/html/header.html'),
+        fetch('/sitesources/html/footer.html')
+    ]).then(([headerResponse, footerResponse]) => {
+        if (!headerResponse.ok || !footerResponse.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return Promise.all([headerResponse.text(), footerResponse.text()]);
+    }).then(([headerData, footerData]) => {
+        document.body.innerHTML = `<div id='main-content'>${document.body.innerHTML}</div><div id='foot-placeholder'></div>`;
+
+        const headerPlaceholder = document.getElementById('header-placeholder');
+        if (headerPlaceholder) {
+            headerPlaceholder.innerHTML = headerData;
+        }
+        const footPlaceholder = document.getElementById('foot-placeholder');
+        if (footPlaceholder) {
+            footPlaceholder.innerHTML = footerData;
+        }
+
+        setTimeout(() => {
+            getFooterHeight();
+        }, 100);
+    }).catch(error => {
+        console.error('Error loading header:', error);
+    });
 });
+window.addEventListener('resize', getFooterHeight);
+
+// ======================================================
+
+function getFooterHeight() {
+    const mainContent = document.getElementById('main-content');
+    const footer = document.getElementById('foot-placeholder');
+
+    if (!mainContent || !footer) return;
+
+    // 计算页脚高度
+    const footerHeight = footer.offsetHeight;
+    console.log(footerHeight);
+
+    // 设置main-content的最小高度
+    mainContent.style.minHeight = `calc(100vh - ${footerHeight}px)`;
+}
