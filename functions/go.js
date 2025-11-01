9832c14ba90a304737180d50f1ff684645f6d085
SuperShadiao <3381949033@qq.com>
2025-11-02 00:23:19 +0800

    更新宝藏UP主列表

--- functions/go.js
+++ functions/go.js
@@ -6,6 +6,7 @@ const goto = {
     "5": "https://space.bilibili.com/1917949964",
     "6": "https://space.bilibili.com/3494361276877525",
     "7": "https://wt.4d4v.fun/about.html",
+    "8": "https://space.bilibili.com/526689535",
 
     "as": "/antiscamming",
     "as_s2": "https://github.com/SuperShadiao/hypixelhelper/wiki/Discord%E9%98%B2%E9%AA%97%E8%AD%A6%E7%A4%BA#%E5%9C%BA%E6%99%AF2-skill-issue%E6%9D%A5%E7%9A%84%E5%B9%B6%E9%82%80%E8%AF%B7%E6%88%91%E5%8A%A0%E5%85%A5%E5%B7%B2%E7%BB%8F%E5%8A%A0%E8%BF%87%E7%9A%84%E6%9C%8D%E5%8A%A1%E5%99%A8"
