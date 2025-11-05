805cef4a2a01d93c0ba857f76bd2fe799001b9f5
SuperShadiao <3381949033@qq.com>
2025-11-05 13:51:10 +0800

    aaa

--- functions/go.js
+++ functions/go.js
@@ -9,7 +9,9 @@ const goto = {
     "8": "https://space.bilibili.com/526689535",
 
     "as": "/antiscamming",
-    "as_s2": "https://github.com/SuperShadiao/hypixelhelper/wiki/Discord%E9%98%B2%E9%AA%97%E8%AD%A6%E7%A4%BA#%E5%9C%BA%E6%99%AF2-skill-issue%E6%9D%A5%E7%9A%84%E5%B9%B6%E9%82%80%E8%AF%B7%E6%88%91%E5%8A%A0%E5%85%A5%E5%B7%B2%E7%BB%8F%E5%8A%A0%E8%BF%87%E7%9A%84%E6%9C%8D%E5%8A%A1%E5%99%A8"
+    "as_s2": "https://github.com/SuperShadiao/hypixelhelper/wiki/Discord%E9%98%B2%E9%AA%97%E8%AD%A6%E7%A4%BA#%E5%9C%BA%E6%99%AF2-skill-issue%E6%9D%A5%E7%9A%84%E5%B9%B6%E9%82%80%E8%AF%B7%E6%88%91%E5%8A%A0%E5%85%A5%E5%B7%B2%E7%BB%8F%E5%8A%A0%E8%BF%87%E7%9A%84%E6%9C%8D%E5%8A%A1%E5%99%A8",
+
+    "100": "https://act.mihoyo.com/sr/event/e20251105return-tb31ya/index.html?utm_source=share&utm_medium=qr&utm_campaign=ingame&inviter=CAM8N2SKXN&mhy_landscape=true&mhy_auth_required=true&mhy_hide_status_bar=true&win_mode=fullscreen"
 }
 
 export function onRequest(context) {
