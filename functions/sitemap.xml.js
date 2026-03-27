830b550743a928679a2e5a1f1ba9eece1eb9d7ca
SuperShadiao <3381949033@qq.com>
2026-03-28 01:02:21 +0800

    update

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -4,7 +4,12 @@ export async function onRequest(context) {
     const host = url.hostname;
 
     const response = await context.next();
-    
-    response.body = response.body.replace("{URL}", host);
-    return response;
+    const text = await response.text();
+
+
+    return new Response(text.replace("{URL}", host), {
+        headers: {
+            "Content-Type": "application/xml; charset=utf-8"
+        }
+    });
 }
