dcf52acf1dd585d592e94ee5a0371026c01baafb
SuperShadiao <3381949033@qq.com>
2026-04-23 00:09:04 +0800

    test

--- functions/datagetter.js
+++ functions/datagetter.js
@@ -1,11 +1,21 @@
 export function onRequest(context) {
   let url = new URL(context.request.url).searchParams.get('url');
   
-  return fetch(new URL( url ), {headers: context.request.headers})
+  let target = new URL( url )
+  let header = new Headers(context.request.headers)
+  if(target.hostname == "api.github.com") {
+    header.append("Authorization", "token " + context.env.github_key)
+  }
+  return fetch(target, {headers: header})
 }
 
 export function onRequestPost(context) {
   let url = new URL(context.request.url).searchParams.get('url');
   
-  return fetch(new URL( url ), {method: "POST", body: context.request.body, headers: context.request.headers})
+  let target = new URL( url )
+  let header = new Headers(context.request.headers)
+  if(target.hostname == "api.github.com") {
+    header.append("Authorization", "token " + context.env.github_key)
+  }
+  return fetch(target, {method: "POST", body: context.request.body, headers: header})
 }
