e10ddc2277292a21d6367ce81beef864545015cc
SuperShadiao <3381949033@qq.com>
2026-06-27 11:19:52 +0800

    update

---
+++ functions/gen_fishe_deploy_blp.js
@@ -0,0 +1,24 @@
+export async function onRequest(context) {
+
+    let res = await fetch("https://api.hypixel.net/v2/guild?id=61a9de838ea8c940a6e5c89c&key=40daffc0-2bf1-4d07-9434-1bc87ca7a880")
+    let json = await res.json()
+
+    let blpBuilderArr = []
+    let blpBuilder = {
+        // "BlacklistedPlayers": []
+    }
+
+    for (let member of json["guild"]["members"]) {
+        let tempObj = {}
+        tempObj[member["uuid"]] = "autofishe"
+        blpBuilderArr.push(tempObj)
+    }
+    blpBuilder["BlacklistedPlayers"] = blpBuilderArr
+
+    return Response.json(blpBuilder)
+
+}
+
+
+
+
